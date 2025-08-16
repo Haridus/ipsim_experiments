777d45eb0fb510709c788a782c80dce2add7a1bb
Haridus <38805854+Haridus@users.noreply.github.com>
2025-08-16 20:11:45 +0300

    Add SCSTR and STEP P-control

---
+++ controls/SCSTR.py
@@ -0,0 +1,109 @@
+import numpy as np
+import os
+import sys
+sys.path.append(os.path.join(os.path.dirname(__file__), "../../../ipsim")) 
+
+from ipsim import *
+from ipsim.models import *
+
+from scipy.integrate import solve_ivp
+
+import numpy as np
+import random as rnd
+from scipy.integrate import solve_ivp
+import matplotlib.pyplot as plt
+from copy import deepcopy
+
+#===============================================================================
+class PID:
+    def __init__(self, setpoint, Kp
+                , *, Ki = 0, Kd = 0, dt = 0
+                , min_value = 0
+                , max_value = 0 ):
+        self.setpoint = setpoint
+        self.Kp = Kp
+        self.Ki = Ki
+        self.Kd = Kd
+        self.dt = dt
+        self.min_value = min_value
+        self.max_value = max_value
+        self.integral = 0
+        self.prev_error = 0
+        self.error = 0
+
+    def __call__(self, meas, manipulated_variable_value = 0):
+        self.error = self.setpoint - meas
+        P = self.Kp*self.error
+
+        I = 0
+        if self.Ki >= 0 and self.dt > 0:
+            self.integral += self.error*self.dt
+            I = self.Ki*self.integral
+
+        D = 0
+        if self.Kd >= 0 and self.dt > 0:
+            derivative = (self.error - self.prev_error)/self.dt
+            D = self.Kd*derivative
+
+        self.prev_error = self.error
+        value = np.clip(P+I+D + manipulated_variable_value, a_min=self.min_value, a_max= self.max_value)
+
+        return value
+
+#-----------------------------------------------------------------------------------    
+def run(process_model, controller = None, *, iteration = 1000):
+    cAs  = []
+    cBs  = []
+    Ts   = []
+    actions = []
+    coolant_node = process_model.nodes()["Coolant"]
+    action = [coolant_node.value("T"), ]
+    for i in range(iteration):
+        state = process_model.step(action = action)
+        cAs.append(state[0])
+        cBs.append(state[1])
+        Ts.append(state[2])
+        
+        if controller is not None:
+            newTc = controller(state[2], coolant_node.value("T") )
+            action[0] = newTc 
+            actions.append(newTc)
+        else:
+            actions.append(coolant_node.value("T"))
+
+    return {"cAs":cAs, "cBs":cBs, "Ts":Ts, "Actions":actions}
+
+def show_metrics(data):
+    plt.figure(figsize=(12,3))
+    plt.subplot(1, 4, 1)
+    plt.plot(data["cAs"])
+    plt.title('Concentration [mol/L]')
+    plt.subplot(1, 4, 2)
+    plt.plot(data["cBs"])
+    plt.title('Concentration [mol/L]')
+    plt.subplot(1, 4, 3)
+    plt.plot(data["Ts"])
+    plt.title('Temperature [K]')
+    plt.subplot(1, 4, 4)
+    plt.plot(data["Actions"])
+    plt.title('Temperature [K]')
+
+if __name__ == "__main__":
+    observer    = ProcessModel.make_common_objerver( [ ("SensorA", "cA")
+                                                   , ("SensorB", "cB")
+                                                   , ("SensorT", "T") ])
+    manipulator = ProcessModel.make_common_manipulator([("Coolant","T"), ])
+
+    #init_state = {"cA":0.9268781731904586, "cB":0, "T":317.7349003254033}
+    init_state = {"cA":0.5, "cB":0, "T":350}
+    scstr = CSTRSimple(  solver = solve_ivp
+                       , observer = observer
+                       , manipulator = manipulator
+                       , init_state = init_state)
+    
+    pid = PID(setpoint=385, Kp=0.03125, Ki=2.842170943040401e-16, Kd = 0.0075, dt=scstr.dt(), min_value=298, max_value=395)
+
+    data = run(scstr, controller=pid)
+    show_metrics(data)
+    plt.show()
+    
---
+++ controls/STEP.py
@@ -0,0 +1,156 @@
+import numpy as np
+import os
+import sys
+sys.path.append(os.path.join(os.path.dirname(__file__), "../../../ipsim")) 
+
+from ipsim import *
+from ipsim.models import *
+
+from scipy.integrate import solve_ivp
+
+import numpy as np
+import random as rnd
+from scipy.integrate import solve_ivp
+import matplotlib.pyplot as plt
+from copy import deepcopy
+
+#===============================================================================
+class STEPMultiloopPControl:
+    def __init__(self, F4_setpoint, P_setpoint, cA3_setpoint, dt = 0.1):
+        self.Kcs   = [0.1, -0.25, 2.0]
+        self.tauis = [1.0, 1.5, 3.0] 
+        self.errn1PC = 0
+        self.KcPC       = 0.7
+        self.TauiPC     = 3
+        self.F4sp_adj   = 0
+        self.F4_setpoint = F4_setpoint
+        self.P_setpoint  = P_setpoint
+        self.cA3_setpoint = cA3_setpoint 
+        self.F4errn1  = 0
+        self.Perrn1   = 0
+        self.cA3errn1 = 0
+        self.dt = dt
+      
+    def __call__(self, meas, mvs = None, *args, **kwds):
+        #return [ X[0], X[1], X[2], X[3]
+        #               , f1, f2, F3.F, F4.F
+        #               , F3.Comp[0], F3.Comp[1], F3.Comp[2]
+        #               , P, Vl ]
+
+        errnPC = (2900 - meas[11])
+        self.F4sp_adj = min(self.F4sp_adj+self.KcPC*( (errnPC-self.errn1PC)+(self.dt*errnPC)/self.TauiPC ), 0)
+        self.errn1PC=errnPC
+
+        F4_setpoint_sv  = self.F4_setpoint
+        P_setpoint_sv   = self.P_setpoint
+        cA3_setpoint_sv = self.cA3_setpoint
+        
+        self.F4_setpoint = self.F4_setpoint + self.F4sp_adj
+
+        F4errn  = self.F4_setpoint  - meas[7]
+        Perrn   = self.P_setpoint   - meas[11]
+        cA3errn = (self.cA3_setpoint - meas[10])*100 # since Kcs for address valves positions from 0 to 100, but here they witheen range 0 to 1
+        
+        F4delu  = self.Kcs[0]*( (F4errn  - self.F4errn1)+(self.dt*F4errn)/self.tauis[0] )
+        Pdelu   = self.Kcs[1]*( (Perrn   - self.Perrn1)+(self.dt*Perrn)  /self.tauis[1] )
+        cA3delu = self.Kcs[2]*( (cA3errn - self.cA3errn1)+(self.dt*cA3errn)/self.tauis[2] )
+        
+        X_target = [ np.clip(mvs[0] + F4delu/100, a_min= 0, a_max=1)
+                   , np.clip(mvs[1] + cA3delu/100, a_min= 0, a_max=1)
+                   , np.clip(mvs[2] + Pdelu/100, a_min= 0, a_max=1)
+        ]
+        X_target.append(mvs[3])
+        
+        self.F4errn1  = F4errn
+        self.Perrn1 = Perrn
+        self.cA3errn1  =cA3errn
+        
+        self.F4_setpoint  = F4_setpoint_sv   
+        self.P_setpoint   = P_setpoint_sv    
+        self.cA3_setpoint = cA3_setpoint_sv
+        
+        return X_target
+
+#===============================================================================
+def show(x, metadata, title = "", *, cols=4, dt=0.1):
+    fig = plt.figure(figsize=(6,6),)
+    fig.suptitle(title)
+    paramsCount = len(metadata)
+    rows = int(paramsCount / cols) + (1 if paramsCount % cols > 0 else 0 )
+    
+    for r in range(0, rows):
+        for c in range(0,cols):
+            index = r*cols + c
+            if index < paramsCount:
+                plt.subplot(rows, cols, index+1)
+                if metadata[index]["range"] is not None:
+                    plt.ylim(metadata[index]["range"][0], metadata[index]["range"][1])
+                plt.plot([i*dt for i in range(len(x))] ,x[:,index])
+                plt.title(metadata[index]["title"])
+    
+    plt.tight_layout()
+    plt.show()
+
+#==============================================================================
+def run(process_model, *, controller = None, time_target=30):
+    dt = process_model.dt()
+    iterations = int( (time_target)/dt)
+    x = np.empty((iterations,13))
+
+    parameters_metadata = (
+          {"parameter":"x1", "range":(0,1), "units": "", "title":"valve 1 pos"}
+         ,{"parameter":"x2", "range":(0,1), "units": "", "title":"valve 2 pos"}
+         ,{"parameter":"x3", "range":(0,1), "units": "", "title":"valve 3 pos"}
+         ,{"parameter":"x4", "range":(0,1), "units": "", "title":"valve 4 pos"}
+         ,{"parameter":"F1", "range":(0,300), "units": "kmol", "title":"Flow 1"}
+         ,{"parameter":"F2", "range":(0,30), "units": "kmol", "title":"Flow 1"}
+         ,{"parameter":"F3", "range":(0,100), "units": "kmol", "title":"Flow 1"}
+         ,{"parameter":"F4", "range":(0,200), "units": "kmol", "title":"Flow 1"}
+         ,{"parameter":"cA", "range":(0,1), "units": "", "title":"mol. frac A"}
+         ,{"parameter":"cB", "range":(0,1), "units": "", "title":"mol. frac B"}
+         ,{"parameter":"cC", "range":(0,1), "units": "", "title":"mol. frac C"}
+         ,{"parameter":"P", "range":(2500,3000), "units": "kPa", "title":"Pressure"}
+         ,{"parameter":"vl",  "units": "", "range":(0,1), "title":"Liq. inv. fill rate"  }
+         )
+
+    valves_positions = process_model.nodes()["ValvesControl"].value("X")
+    action = [valves_positions,]
+    for _ in range(iterations):
+        if controller is not None:
+            state = process_model.step(action = action)
+            valves_positions = controller(state, valves_positions)
+            action[0] = valves_positions
+        else:
+            state = process_model.step()
+
+        x[_] = state
+
+    return x, parameters_metadata
+
+#===================================================================================
+if __name__ == "__main__":
+    def step_observer(model, state):
+        f1 = state['Sensorf1']['f1'] #1
+        f2 = state['Sensorf2']['f2'] #1
+        F3 = state['SensorF3']['F3'] #3
+        F4 = state['SensorF4']['F4'] #2
+        Vl = state['SensorVl']['Vl'] #1
+        P  = state['SensorP']['P']   #1
+        X  = state['SensorX']['X']   #4
+
+        return [ X[0], X[1], X[2], X[3]
+               , f1, f2, F3.F, F4.F
+               , F3.Comp[0], F3.Comp[1], F3.Comp[2]
+               , P, Vl ]
+    
+    manipulator = ProcessModel.make_common_manipulator([("ValvesControl","X"), ])
+    step = STEP(  solver = solve_ivp
+                , observer = step_observer
+                , manipulator = manipulator)
+    
+    multiloop_control = STEPMultiloopPControl(F4_setpoint=100.00318503097513, P_setpoint=2700.165624248256,cA3_setpoint=0.3870675068985829,dt=step.dt())
+
+    data, metadata = run(step, controller=multiloop_control)
+    show(data, metadata=metadata)
+    plt.show()
+    
