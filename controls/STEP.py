d345504710f94106867e0d246db8b2bbc50bb227
Haridus <38805854+Haridus@users.noreply.github.com>
2025-10-18 21:56:15 +0300

    update step control

--- controls/STEP.py
+++ controls/STEP.py
@@ -14,7 +14,7 @@ from copy import deepcopy
 
 #===============================================================================
 class STEPMultiloopPControl:
-    def __init__(self, F4_setpoint, P_setpoint, cA3_setpoint, dt = 0.1):
+    def __init__(self, F4_setpoint, P_setpoint, cA3_setpoint, dt = 0.1, process = None):
         self.Kcs   = [0.1, -0.25, 2.0]
         self.tauis = [1.0, 1.5, 3.0] 
         self.errn1PC = 0
@@ -28,34 +28,29 @@ class STEPMultiloopPControl:
         self.Perrn1   = 0
         self.cA3errn1 = 0
         self.dt = dt
+        self.proress = process
       
     def __call__(self, meas, mvs = None, *args, **kwds):
-        #return [ X[0], X[1], X[2], X[3]
-        #               , f1, f2, F3.F, F4.F
-        #               , F3.Comp[0], F3.Comp[1], F3.Comp[2]
-        #               , P, Vl ]
-
-        errnPC = (2900 - meas[11])
+        errnPC = (2900 - meas[2])
         self.F4sp_adj = min(self.F4sp_adj+self.KcPC*( (errnPC-self.errn1PC)+(self.dt*errnPC)/self.TauiPC ), 0)
         self.errn1PC=errnPC
 
         F4_setpoint = self.F4_setpoint + self.F4sp_adj
+        F4errn  = F4_setpoint  - meas[1]
+        Perrn   = self.P_setpoint   - meas[2]
+        cA3errn = (self.cA3_setpoint - meas[0])*100 # since Kcs for address valves positions from 0 to 100, but here they witheen range 0 to 1
+        
+        F4delu  = self.Kcs[0]*( (F4errn  - self.F4errn1)  + (self.dt*F4errn)/self.tauis[0] )
+        Pdelu   = self.Kcs[1]*( (Perrn   - self.Perrn1)   + (self.dt*Perrn)  /self.tauis[1] )
+        cA3delu = self.Kcs[2]*( (cA3errn - self.cA3errn1) + (self.dt*cA3errn)/self.tauis[2] )
+        
+        X = self.proress.nodes()['SensorX'].value("X")
 
-        F4errn  = F4_setpoint  - meas[7]
-        Perrn   = self.P_setpoint   - meas[11]
-        cA3errn = (self.cA3_setpoint - meas[10])*100 # since Kcs for address valves positions from 0 to 100, but here they witheen range 0 to 1
-        
-        F4delu  = self.Kcs[0]*( (F4errn  - self.F4errn1)+(self.dt*F4errn)/self.tauis[0] )
-        Pdelu   = self.Kcs[1]*( (Perrn   - self.Perrn1)+(self.dt*Perrn)  /self.tauis[1] )
-        cA3delu = self.Kcs[2]*( (cA3errn - self.cA3errn1)+(self.dt*cA3errn)/self.tauis[2] )
-        
-        X_target = [ np.clip(mvs[0] + F4delu/100, a_min= 0, a_max=1) #since Kcs and tauis is specified for X form 0 to 100% and here X from 0 to 1dd
-                   , np.clip(mvs[1] + cA3delu/100, a_min= 0, a_max=1)
-                   , np.clip(mvs[2] + Pdelu/100, a_min= 0, a_max=1)
+        X_target = [ np.clip(X[0] + F4delu/100, a_min= 0, a_max=1) #since Kcs and tauis is specified for X form 0 to 100% and here X from 0 to 1dd
+                   , np.clip(X[1] + cA3delu/100, a_min= 0, a_max=1)
+                   , np.clip(X[2] + Pdelu/100, a_min= 0, a_max=1)
         ]
-        X_target.append(mvs[3])
-        #print(f"X:{X_target}")
-        
+
         self.F4errn1  = F4errn
         self.Perrn1   = Perrn
         self.cA3errn1 = cA3errn
@@ -86,73 +81,59 @@ def show(x, metadata, title = "", *, cols=4, dt=0.1):
 def run(process_model, *, controller = None, time_target=30):
     dt = process_model.dt()
     iterations = int( (time_target)/dt)
-    x = np.empty((iterations,13))
+    x = np.empty((iterations,3))
 
     parameters_metadata = (
-          {"parameter":"x1", "range":(0,1), "units": "", "title":"valve 1 pos"}
-         ,{"parameter":"x2", "range":(0,1), "units": "", "title":"valve 2 pos"}
-         ,{"parameter":"x3", "range":(0,1), "units": "", "title":"valve 3 pos"}
-         ,{"parameter":"x4", "range":(0,1), "units": "", "title":"valve 4 pos"}
-         ,{"parameter":"F1", "range":(0,300), "units": "kmol", "title":"Flow 1"}
-         ,{"parameter":"F2", "range":(0,30), "units": "kmol", "title":"Flow 2"}
-         ,{"parameter":"F3", "range":(0,100), "units": "kmol", "title":"Flow 3"}
-         ,{"parameter":"F4", "range":(0,200), "units": "kmol", "title":"Flow 4"}
-         ,{"parameter":"cA", "range":(0,1), "units": "", "title":"mol. frac A"}
-         ,{"parameter":"cB", "range":(0,1), "units": "", "title":"mol. frac B"}
-         ,{"parameter":"cC", "range":(0,1), "units": "", "title":"mol. frac C"}
-         ,{"parameter":"P", "range":(2500,3000), "units": "kPa", "title":"Pressure"}
-         ,{"parameter":"vl",  "units": "", "range":(0,1), "title":"Liq. inv. fill rate"  }
+          {"parameter":"cA", "range":(0,1), "units": "", "title":"mol. frac A"}
+         ,{"parameter":"F4", "range":(80,140), "units": "kmol", "title":"Flow 4"}
+         ,{"parameter":"P", "range":(2600,3000), "units": "kPa", "title":"Pressure"}
          )
-
-    valves_positions = process_model.nodes()["ValvesControl"].value("X")
-    action = [valves_positions,]
+    
+    state  = process_model.step()
     for _ in range(iterations):
         if controller is not None:
+            action = controller(state)
             state = process_model.step(action = action)
-            valves_positions = controller(state, valves_positions)
-            action[0] = valves_positions
         else:
             state = process_model.step()
-
         x[_] = state
-
     return x, parameters_metadata
 
 #===================================================================================
+config = {"sampling_time": 0.1}
+
+def process_model_creator( dt = config.get("sampling_time", 0.1), initial_state = None ):
+        from ipsim.models import STEP
+
+        def step_observer(model, state):
+            F3 = state['SensorF3']['F3'] #3
+            F4 = state['SensorF4']['F4'] #2
+            P  = state['SensorP']['P']   #1
+            
+            return np.array([F3.Comp[STEPFlow.A], F4.F, P, ])
+        observer = step_observer
+
+        def step_manipulator(model, action):
+            uX = model.nodes()["ValvesControl"].value("X")
+            uX[0] = action[0]
+            uX[1] = action[1]
+            uX[2] = action[2]
+
+        manipulator = step_manipulator
+
+        solver = lambda f, ts, x, u, p: solve_ivp(f, ts, x, args = (u, p, ), method='LSODA')
+        process =  STEP(solver=solver, observer=observer, manipulator=manipulator, dt=0.1)
+        return process
+
 if __name__ == "__main__":
-    def step_observer(model, state):
-        f1 = state['Sensorf1']['f1'] #1
-        f2 = state['Sensorf2']['f2'] #1
-        F3 = state['SensorF3']['F3'] #3
-        F4 = state['SensorF4']['F4'] #2
-        Vl = state['SensorVl']['Vl'] #1
-        P  = state['SensorP']['P']   #1
-        X  = state['SensorX']['X']   #4
+    step = process_model_creator(dt=0.1)
+    for _ in range(100):
+        step.step()
+    print(step.step())
 
-        return [ X[0], X[1], X[2], X[3]
-               , f1, f2, F3.F, F4.F
-               , F3.Comp[0], F3.Comp[1], F3.Comp[2]
-               , P, Vl ]
-    
-    manipulator = ProcessModel.make_common_manipulator([("ValvesControl","X"), ])
-    solver = lambda f, ts, x, u, p: solve_ivp(f, ts, x, args = (u, p, ), method='LSODA')
-
-    step = STEP(  solver = solver
-                , observer = step_observer
-                , manipulator = manipulator)
-    
     multiloop_control = None
-    multiloop_control = STEPMultiloopPControl(F4_setpoint=100, P_setpoint=2700, cA3_setpoint=0.38, dt=step.dt())
-    #F_4 = 130, P= 2850, yA3 = 0.63
-    #multiloop_control = STEPMultiloopPControl(F4_setpoint=130.00, P_setpoint=2850.0, cA3_setpoint=0.63, dt=step.dt())
-    def test_Control(self, meas, mvs = None, *args, **kwds):
-        return [meas[0], meas[1], 1,meas[3], ]
-        #return [meas[0], 0.4, meas[2],meas[3], ]
-        #return [0.76, meas[1], meas[2],meas[3], ]
-        #return [meas[0], 0, meas[2],meas[3], ]
-    multiloop_control = test_Control
-    
-    data, metadata = run(step, controller=multiloop_control)
+    #multiloop_control = STEPMultiloopPControl(F4_setpoint=99.99991893665879, P_setpoint=2700.1477279941, cA3_setpoint=0.4699865552748352, dt=step.dt())
+    multiloop_control = STEPMultiloopPControl(F4_setpoint=130.00, P_setpoint=2850.0, cA3_setpoint=0.63, dt=step.dt(), process=step)
+    data, metadata = run(step, controller=multiloop_control, time_target=30)
     show(data, metadata=metadata)
     plt.show()
-    
